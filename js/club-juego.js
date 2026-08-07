71c9f7f86deeda0bc03fa0a584e86fed206d4df2
naakita <50963742+naakita@users.noreply.github.com>
2026-08-07 20:51:18 -0300

    feat: cargar laboratorio de armado para Juniors

--- js/club-juego.js
+++ js/club-juego.js
@@ -1,9 +1,9 @@
-/* Cargador compatible del Club B.E.I. · 20260806f */
+/* Cargador compatible del Club B.E.I. · 20260807c */
 (function(){
   'use strict';
   var src=(document.currentScript&&document.currentScript.src)||'';
   var base=src.slice(0,src.lastIndexOf('/')+1);
-  ['club-juego-1.js','club-juego-2.js','club-juego-3.js','club-juego-4.js','club-mejoras.js'].forEach(function(file){
-    document.write('<script src="'+base+file+'?v=20260806f"><\/script>');
+  ['club-juego-1.js','club-juego-2.js','club-juego-3.js','club-juego-4.js','club-mejoras.js','club-pc-lab.js'].forEach(function(file){
+    document.write('<script src="'+base+file+'?v=20260807c"><\/script>');
   });
 })();
